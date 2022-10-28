457b9f179e96862825a8860bf2a9da9d1b683604
Marcos Paiva <marcospaivajr7@gmail.com>
2022-10-28 10:36:37 -0300

    fix: atualizando o projeto para PHP - navbar_top

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <title>Canvas de Usabilidade</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <?php include "./php/navbar_top.php"; ?>
+
+  <div class="titulo">
+    <h1>Planeje a Avaliação de Usabilidade</h1>
+  </div>
+
+
+  <div class="container-fluid text-center">    
+    <div class="row content">
+      <div class="col-sm-2 sidenav">
+        <p><strong>Casos de sucesso:</strong></p>
+        <p><a href="Case de Sucesso - OLX.pdf">OLX</a></p>
+        <p>“Com o investimento nas ferramentas do LinkedIn, a OLX passou a contar
+          com mais autonomia para fazer a triagem e selecionar os candidatos para um
+          processo seletivo. Os resultados superaram a expectativa da empresa, pois
+          a marca OLX passou a ser a mais desejada entre os candidatos de várias áreas.”<br/>
+          <small>Simone Lima
+          (Gerente de RH)</small></p>
+          
+        <p><a href="Case de Sucesso - Autopass.pdf">Autopass</a></p>
+        <p>
+                    “Nosso processo de seleção e contratação não era
+            efetivo. Com o acesso a toda base do LinkedIn, a busca
+            por profissionais ficou mais rápida e fácil. Os filtros
+            disponíveis no Recruiter facilitam a nossa vida”. <br/>
+            <small>Roberta Fernandes de Souza (Gerente de Gente e Gestão
+            da Autopass).</small>
+        </p>
+      </div>
+
+      <div class="col-sm-8 text-center"> 
+        <h3>O que é o Canvas de Usabilidade?</h3>
+        <br>
+        <p>Usabilidade é um atributo de qualidade de software relacionado à facilidade de se utilizá-lo e esse atributo é relevante para vários tipos de sistemas. Para verificar se o produto ou serviço pretendido atende aos atributos de usabilidade exigidos em relação aos usuários esperados, é feita uma avaliação de usabilidade. A avaliação de usabilidade é um nome genérico para um grupo de métodos baseados na avaliação e inspeção ou exame relacionado com aspectos de usabilidade da interface com o usuário. Existem várias alternativas para se realizar a avaliação da usabilidade de uma solução. OCAU (O canvas de Avaliação de Usabilidade) é um artefato desenvolvido com a intenção de facilitar o planejamento da avaliação de usabilidade possuindo 8 dimensões.</p>
+        <hr>
+        <h3>Test</h3>
+        <img src="assets/img/imagemusa.png" alt="" id="imgusabilidade">
+        <br>
+        <br>
+        <h3 >Como usar o Canvas de Usabilidade?</h3>
+        <br>
+        <table >
+          <ul class="list-group">
+            <li class="list-group-item"><strong>1.Defina a Solução: O que será avaliado?</strong></li>
+            <li class="list-group-item">2.Defina os Requisitos: Qual requisito ou funcionalidade será avaliado?</li>
+            <li class="list-group-item">3.Defina os Usuários: Quem são os participantes da avaliação?</li>
+            <li class="list-group-item">4. Defina a(s) Tarefas: Quais tarefas os participantes irão realizar?</li>
+            <li class="list-group-item">5. Defina o Contexto: Em quais contextos estão inseridos?</li>
+            <li class="list-group-item">6. Defina a(s) Alternativas: Quais alternativas serão utilizadas?</li>
+            <li class="list-group-item">7. Defina os Critérios: Quais são os critérios utilizados?</li>
+            <li class="list-group-item">8. Defina as Métricas: Quais são as métricas utilizados?</li>
+          </ul>
+        </table>
+      </div>
+      <div class="col-sm-2 sidenav">
+        <div class="well">
+          <h4>Baixe aqui o Manual</h4>
+        </div>
+        <button type="button" class="btn btn-primary">Manual</button>
+      </div>
+    </div>
+  </div>
+
+
+<footer class="container-fluid text-center">
+  <p>Canvas de Usabilidade, 2022</p>
+</footer>
+
+</body>
+</html>
---
+++ php/navbar_top.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <img class="navbar_logo" src="./assets/img/logo.png" alt="logo">
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- <div class="header">
+  <header>
+    <img src="./assets/img/logou.png" alt="">
+  </header>
+</div>
+<hr> -->
